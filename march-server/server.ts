5dad32b6c6633ebf83e265f4b55384615387460b
MaRcH <marchyhh@gmail.com>
2024-02-06 11:44:00 +0800

    creating the server and rememberyarn

--- march-server/server.ts
+++ march-server/server.ts
@@ -1,14 +1,23 @@
+import cors from "cors";
 import express from "express";
-import expressSession from "express-session";
+
 
 
 const app = express();
+
+app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}));
-app.use(
-    expressSession({
-      secret: "gyukj%^&*(*UYTGYHUJYT&*YHIUGYGYI",
-      resave: true,
-      saveUninitialized: true,
-    })
-  );
+import Knex from "knex";
+
+const knexConfig = require("./knexfile")
+const knex = Knex(knexConfig[process.env.NODE_ENV || "development"]);
+
+const PORT = 7777;
+
+app.get("/sexy", (req,res)=> res.send("You a so sexy"))
+
+
+app.listen(PORT, ()=>{
+  console.log(`App running at http://localhost:{PORT}`)
+});
