110461448f851cc9278d31be25b2acba2b8df96a
atehtchan <thetac@gmail.com>
2024-02-07 10:22:15 +0800

    fix file path

--- my-app/src/Components/UploadPage.tsx
+++ my-app/src/Components/UploadPage.tsx
@@ -18,30 +18,6 @@ export default function UploadPage() {
     };
 
     return (
-<<<<<<< HEAD
-    <>
-    <label>
-        請選擇捐贈物品： 
-        <select name="selectDonate">
-            <option value="">請選擇</option> 
-            <option value="wheelchair">輪椅</option> 
-            <option value="electric_wheelchair">電動輪椅</option>
-            <option value="crutch">拐杖</option>
-            <option value="walker_frame">步行架</option>
-            <option value="bath_chair">沐浴椅</option>
-            <option value="nutritional_supplement">營養食品</option>
-            <option value="adult_diapers">成人紙尿片</option>
-            <option value="wipes">濕紙巾</option>
-            <option value="sphygmomanometer">血壓計</option>
-            <option value="paramount_bed">護理床</option>
-            <option value="lotion">潤膚霜</option>
-            <option value="others">其他</option>
-        </select>
-        數量 : <input />
-        <button onClick={()=> {}}>add</button>
-    </label>
-    </>)
-=======
         <div className='uploadForm'>
             <form className='selectItem' onSubmit={handleSubmit}>
                 <label>
@@ -71,6 +47,5 @@ export default function UploadPage() {
             </form>
         </div >
     );
->>>>>>> 481b1937957ba12f9db53ce8a95799daab734eda
 }
 
