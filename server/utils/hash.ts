93cb4c67434797c7a10bb4289fee87e9da7835e6
Edgarchung <edgar.chungmantik@gmail.com>
2024-02-07 10:28:10 +0800

    replace hash.ts

--- server/utils/hash.ts
+++ server/utils/hash.ts
@@ -1,17 +1,33 @@
-import { compare, hash } from "bcrypt";
+import { compare, hash } from "bcryptjs"
+import * as bcrypt from "bcryptjs"
 
-const saltRounds = 16;
 
-export async function hashPassword(password_input:string){
-    let hashed = await hash(password_input, saltRounds);
+const ROUND = 12
 
-    return hashed
+export function hashPassword(password: string): Promise<string> {
+    return hash(password, ROUND)
 }
 
-export async function comparePassword(
-    password_input:string,
+// doctor Adam 
+export async function hashedPassword(plainPassword: string) {
+    const hash: string = await bcrypt.hash(plainPassword, ROUND);
+    return hash;
+}
+
+export function comparePassword(options: {
+    password: string
     password_hash: string
-){
-    let result = await compare(password_input, password_hash);
-    return result
+}): Promise<boolean> {
+    return compare(options.password, options.password_hash)
+}
+
+export async function checkPassword(options: {
+    plainPassword: string;
+    hashedPassword: string;
+}) {
+    const isMatched: boolean = await bcrypt.compare(
+        options.plainPassword,
+        options.hashedPassword
+    );
+    return isMatched;
 }
