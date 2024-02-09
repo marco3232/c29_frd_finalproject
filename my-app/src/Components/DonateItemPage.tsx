601a4edf3e65f5e26223b728994d97c56590fc86
Edgarchung <edgar.chungmantik@gmail.com>
2024-02-09 13:48:21 +0800

    nothing change

--- my-app/src/Components/DonateItemPage.tsx
+++ my-app/src/Components/DonateItemPage.tsx
@@ -17,169 +17,169 @@ export default function DonateItemPage() {
         <span className={styles.header}>器材募集 </span>
         <h3>現接受捐贈以下物品</h3>
         <div className={styles.cards}>
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/輪椅.webp"
-            style={{ objectFit: 'contain' }} 
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              輪椅
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/輪椅.webp"
+              style={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                輪椅
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
 
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/電動輪椅.webp"
-            style={{ objectFit: 'contain' }} 
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              電動輪椅
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/電動輪椅.webp"
+              style={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                電動輪椅
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
 
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/四腳叉.webp"
-            style={{ objectFit: 'contain' }} 
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            四腳叉
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/四腳叉.webp"
+              style={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                四腳叉
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
-        
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/穿手拐杖.webp"
-            style={{ objectFit: 'contain' }} 
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            穿手拐杖
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
-        
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/步行架.webp"
-            style={{ objectFit: 'contain' }} 
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            步行架
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
 
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/沐浴椅.webp"
-            style={{ objectFit: 'contain' }} 
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            沐浴椅
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/穿手拐杖.webp"
+              style={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                穿手拐杖
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
 
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/便椅.webp"
-            style={{ objectFit: 'contain' }} 
-
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            便椅
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/步行架.webp"
+              style={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                步行架
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
 
-        <Card  sx={{ width: 300 }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="300"
-            image="assets/donate_items_img/高背椅.webp"
-            style={{ objectFit: 'contain' }} 
-
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-            高背椅
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/沐浴椅.webp"
+              style={{ objectFit: 'contain' }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                沐浴椅
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography> */}
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
-        
-       
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
+
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/便椅.webp"
+              style={{ objectFit: 'contain' }}
+
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                便椅
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography> */}
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
+
+          <Card sx={{ width: 300 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="300"
+              image="assets/donate_items_img/高背椅.webp"
+              style={{ objectFit: 'contain' }}
+
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                高背椅
+              </Typography>
+              {/* <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography> */}
+            </CardContent>
+            <CardActions></CardActions>
+          </Card>
+
+
         </div>
         <button onClick={() => navigate('/Upload')}>按此捐贈</button>
       </div>
