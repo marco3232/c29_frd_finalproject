3c04615f850528b0c5ac4bd57e27e66f75fa0c40
marco.wong <s1993618@yahoo.com.hk>
2024-02-15 16:16:53 +0800

    waiting submit button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import NotFoundPage from "./Page/NotFoundPage";
 import NavBarControl from "./Components/NavBars";
-import UploadPage from './Page/UploadPage';
+import UploadPage from "./Page/UploadPage";
 import DonateItemPage from "./Page/DonateItemPage";
 import RegisterForm from "./Components/Register";
 import TransactionPage from "./Components/TransactionPage";
@@ -9,17 +9,13 @@ import { LoginForm } from "./Components/LoginForm";
 import { AuthGuard } from "./utils/authGuard";
 import { useState } from "react";
 
-
-
-
 // --------------------------------------------------------------------------------
 
 function App() {
   const location = useLocation();
   const shouldShowNavBar = location.pathname !== "/notFoundPage";
   const shouldShowWelcomePage = location.pathname === "/";
-  const [isLoggedIn, setIsLoggedIn] = useState(Boolean)
-
+  const [isLoggedIn, setIsLoggedIn] = useState(Boolean);
 
   // const checkLoginStatus = () => {
   //   const token = localStorage.getItem('token'); // Retrieve token from localStorage
@@ -50,23 +46,19 @@ function App() {
         </div>
       )}
 
-
       <Routes>
         <Route path="/Login" element={<LoginForm />} />
         <Route path="/Register" element={<RegisterForm />} />
         <Route path="/notFoundPage" element={<NotFoundPage />} />
+        <Route path="/Upload" element={<UploadPage />} />
+        <Route path="/Donate" element={<DonateItemPage />} />
+        <Route path="/Transaction" element={<TransactionPage />} />
         <Route path="/" element={""} />
 
-
-        <Route element={<AuthGuard />} >
-          <Route path="/Upload" element={<UploadPage />} />
-          <Route path="/Donate" element={<DonateItemPage />} />
-          <Route path="/Transaction" element={<TransactionPage />} />
-        </Route>
+        <Route element={<AuthGuard />}></Route>
       </Routes>
 
       <br />
-
     </div>
   );
 }
