808a13b53695635d12bb2357e321742dbbf0a17a
atehtchan <thetac@gmail.com>
2024-02-19 11:14:23 +0800

    merge

--- my-app/src/hook/dataAPI.ts
+++ my-app/src/hook/dataAPI.ts
@@ -54,37 +54,3 @@ export async function addNewItems(
   let resp = await res.json();
   return resp.message;
 }
-
-export async function addLogistic(
-  room_input?: string,
-  building_input?: string,
-  street_input?: string,
-  district_input?: string,
-  contact_number_input?: string,
-  contact_name_input?: string,
-  confirmed_date_input?: string,
-  confirmed_session_input?: string,
-  user_id_input?: number
-) {
-  const res = await fetch(`${source}/logistic`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization":`Bearer ${localStorage.getItem('token')}`
-    },
-    body: JSON.stringify({
-      room: room_input,
-      building: building_input,
-      street: street_input,
-      district: district_input,
-      contact_number: contact_number_input,
-      contact_name: contact_name_input,
-      confirmed_date: confirmed_date_input,
-      confirmed_session: confirmed_session_input,
-      user_id: user_id_input,
-    }),
-  });
-
-  let resp = await res.json();
-  return resp.message;
-}
