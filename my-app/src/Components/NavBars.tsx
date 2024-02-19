eb1fb21f9f5125cc6b6299da244a11b0bb06495e
Edgarchung <edgar.chungmantik@gmail.com>
2024-02-19 11:29:57 +0800

    update

--- my-app/src/Components/NavBars.tsx
+++ my-app/src/Components/NavBars.tsx
@@ -38,7 +38,6 @@ function OffcanvasExample() {
                                     title="服務項目"
                                     id={`offcanvasNavbarDropdown-expand-${expandSize}`}
                                 >
-                                    <NavDropdown.Item onClick={() => navigate('/Register')}>註冊用戶</NavDropdown.Item>
                                     <NavDropdown.Item onClick={() => navigate('/Upload')}>捐贈物資</NavDropdown.Item>
                                     <NavDropdown.Item onClick={() => navigate('/Transaction')}>Transaction</NavDropdown.Item>
                                     <NavDropdown.Item onClick={() => navigate('/')}>testing2</NavDropdown.Item>
