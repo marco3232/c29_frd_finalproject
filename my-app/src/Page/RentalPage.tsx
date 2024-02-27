b05173e8a7670998aaef6fb46896657f13ec037d
Edgarchung <edgar.chungmantik@gmail.com>
2024-02-27 18:10:14 +0800

    merge

--- my-app/src/Page/RentalPage.tsx
+++ my-app/src/Page/RentalPage.tsx
@@ -38,65 +38,61 @@ export function RentalPage() {
 
   return (
     <>
-      {RentListData &&
-        Array.isArray(RentListData) &&
-        RentListData.map(
-          (
-            entry: {
-              donate_item_id: number;
-              item_name: string;
-              deposit_charge: number;
-              rent_charge: number;
-              image: string;
-            },
-            index
-          ) => (
-            <Row key={index}>
-              <Col md={6} className="mb-2">
-                <CardMedia
-                  component="img"
-                  alt="green iguana"
-                  height="200"
-                  image={entry.image}
-                  style={{ objectFit: "contain" }}
-                />
-                <Button onClick={toggleShowA} className="mb-2">
-                  <strong>{entry.item_name}</strong>
-                </Button>
-                <Toast show={showA} onClose={toggleShowA}>
-                  <Toast.Header>
-                    <img
-                      src="holder.js/20x20?text=%20"
-                      className="rounded me-2"
-                      alt=""
+      <div className="rentalPageControl">
+        <div className="rentalPageContainer">
+          {RentListData &&
+            Array.isArray(RentListData) &&
+            RentListData.map(
+              (
+                entry: {
+                  donate_item_id: number;
+                  item_name: string;
+                  deposit_charge: number;
+                  rent_charge: number;
+                  image: string;
+                },
+                index
+              ) => (
+                <Row className="rentalPageCardControl" key={index}>
+                  <Col className="rentalPageCard">
+                    <CardMedia
+                      className="rentalPageCardImg"
+                      component="img"
+                      alt="green iguana"
+                      height="200"
+                      image={entry.image}
+                      style={{ objectFit: "contain" }}
                     />
-                    <strong className="me-auto">Deposit:</strong>
-                    <small>{entry.deposit_charge}元</small>
-                  </Toast.Header>
-                  <Toast.Header>
-                    <img
-                      src="holder.js/20x20?text=%20"
-                      className="rounded me-2"
-                      alt=""
-                    />
-                    <strong className="me-auto">Rent:</strong>
-                    <small>{entry.rent_charge}元/MONTH</small>
-                  </Toast.Header>
-                  <Toast.Body>
-                    <div className="mb-3">
-                      <Form.Check // prettier-ignore
-                        onChange={() => updateDonateItem(entry.donate_item_id)}
-                        checked={donateItemIds.has(entry.donate_item_id)}
-                      />
-                    </div>
-                  </Toast.Body>
-                </Toast>
-              </Col>
-              {/* <-----------------------------------------------------> */}
-            </Row>
-          )
-        )}
+                    <Toast >
+                      <Toast.Header>
+                        <strong className="me-auto">Item:</strong>
+                        <small>{entry.item_name}</small>
+                      </Toast.Header>
+                      <Toast.Header>
+                        <strong className="me-auto">Deposit:</strong>
+                        <small>{entry.deposit_charge}元</small>
+                      </Toast.Header>
+                      <Toast.Header>
+                        <strong className="me-auto">Rent:</strong>
+                        <small>{entry.rent_charge}元/MONTH</small>
+                      </Toast.Header>
+                      <Toast.Body>
+                        <div className="mb-3">
+                          <Form.Check // prettier-ignore
+                            onChange={() => updateDonateItem(entry.donate_item_id)}
+                            checked={donateItemIds.has(entry.donate_item_id)}
+                          />
+                        </div>
+                      </Toast.Body>
+                    </Toast>
+                  </Col>
+                  {/* <-----------------------------------------------------> */}
+                </Row>
+              )
+            )}
+        </div>
 
+      </div>
       <button onClick={submit}>next</button>
     </>
   );
