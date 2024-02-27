01e6035a309590ee8b933f1964a51989f48f766b
Edgarchung <edgar.chungmantik@gmail.com>
2024-02-27 16:27:06 +0800

    merge

--- my-app/src/hook/checkoutAPI.ts
+++ my-app/src/hook/checkoutAPI.ts
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-import { CheckOutTransactionType, CheckInType } from "../slice/checkOutSlice";
+// import { CheckOutTransactionType } from "../slice/checkOutSlice";
 
 
 const source = "http://localhost:8080";
 
 export function useCheckOutInfo() {
-    const { isLoading, error, data, isFetching} = useQuery ({
-        queryKey:["checkouts"],
+    const { isLoading, error, data, isFetching } = useQuery({
+        queryKey: ["checkouts"],
         queryFn: async () => {
             const res = await fetch(`${source}/checkout`, {
-                headers:{
+                headers: {
                     "Authorization": `Bearer ${localStorage.getItem("token")}`,
                 },
             });
@@ -21,7 +21,7 @@ export function useCheckOutInfo() {
     if (isLoading || isFetching) return "Data Loading";
     if (error) {
         return "Error";
-    } 
+    }
     if (!data) {
         return []
     }
@@ -29,14 +29,14 @@ export function useCheckOutInfo() {
 }
 
 export async function addCheckOut(
-    rentalList: CheckInType[],
-    checkoutTransaction: CheckOutTransactionType
+    checkInIds: number[],
+    // checkoutTransaction: CheckOutTransactionType
 ) {
     const body = {
-        rentalList,
-        checkoutTransaction
+        checkInIds,
+        // checkoutTransaction
     }
-    console.log({body})
+    console.log({ body })
     const res = await fetch(`${source}/checkout`, {
         method: "POST",
         headers: {
--- my-app/src/slice/checkOutSlice.ts
+++ my-app/src/slice/checkOutSlice.ts
@@ -1,59 +1,59 @@
 
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+// import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-// ----------------------------------------------------------------
+// // ----------------------------------------------------------------
 
-export interface CheckOutTransactionType {
-    room: string;
-    building: string;
-    street: string;
-    district: string;
-    contact_number: string;
-    contact_name: string;
-    confirmed_date: string;
-    confirmed_session: string;
-  }
+// export interface CheckOutTransactionType {
+//     room: string;
+//     building: string;
+//     street: string;
+//     district: string;
+//     contact_number: string;
+//     contact_name: string;
+//     confirmed_date: string;
+//     confirmed_session: string;
+// }
 
-  export interface CheckOutState {
-    donateItemIds : number[]
-    checkoutTransaction: CheckOutTransactionType
+// export interface CheckOutState {
+//     donateItemIds: number[]
+//     checkoutTransaction: CheckOutTransactionType
 
-  }
+// }
 
-// ---------------------------------------------------------------
+// // ---------------------------------------------------------------
 
-const initialState: CheckOutState = {
-    donateItemIds: [],
-    checkoutTransaction: {
-        room: '',
-        building: '',
-        street: '',
-        district: '',
-        contact_number: '',
-        contact_name: '',
-        confirmed_date: '',
-        confirmed_session: ''
-    }
-}
+// const initialState: CheckOutState = {
+//     donateItemIds: [],
+//     checkoutTransaction: {
+//         room: '',
+//         building: '',
+//         street: '',
+//         district: '',
+//         contact_number: '',
+//         contact_name: '',
+//         confirmed_date: '',
+//         confirmed_session: ''
+//     }
+// }
 
-// ---------------------------------------------------------------
+// // ---------------------------------------------------------------
 
-export const checkOutSlice = createSlice({
-    name: "checkout",
-    initialState,
-    reducers:{
-        updateRentalList: (state: CheckOutState, action : PayloadAction<CheckInType[]>) => {
-            state.rentalList = action.payload
-        },
-        updateCheckOutTransaction: (state: CheckOutState, action : PayloadAction<CheckInType[]>) => {
-            state.rentalList = action.payload
-        },
-        clearForm: (state: CheckOutState) => {
-            state = initialState
-        },
-    },
+// export const checkOutSlice = createSlice({
+//     name: "checkout",
+//     initialState,
+//     reducers: {
+//         updateRentalList: (state: CheckOutState, action: PayloadAction<CheckInType[]>) => {
+//             state.rentalList = action.payload
+//         },
+//         updateCheckOutTransaction: (state: CheckOutState, action: PayloadAction<CheckInType[]>) => {
+//             state.rentalList = action.payload
+//         },
+//         clearForm: (state: CheckOutState) => {
+//             state = initialState
+//         },
+//     },
 
-});
+// });
 
-export default checkOutSlice.reducer;
-export const {updateRentalList, updateCheckOutTransaction, clearForm} = checkOutSlice.actions;
+// export default checkOutSlice.reducer;
+// export const { updateRentalList, updateCheckOutTransaction, clearForm } = checkOutSlice.actions;
